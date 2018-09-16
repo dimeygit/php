b7e1da5d3e61a5e589ce1aaea8eeb10577d3182b
dimey <mr.dimey@gmail.com>
2018-09-16 20:05:33 +0300

    Create project tree

---
+++ view/helloView.php
@@ -0,0 +1,16 @@
+<!--
+
+    This is just a view that defines how the page looks.
+    It doesn't do any logic, access the DB, or anything else.
+
+    It is completely dumb.
+
+-->
+
+<h1 style="color: deeppink">
+    Hello <?php print $person->name; ?>
+</h1>
+
+<p>
+    You like everything to be <?php print $person->fav_colour; ?> ;)
+</p>
