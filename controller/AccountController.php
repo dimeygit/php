5c557de53d661b5b7aa8517bbe68aa40afc85a1f
Dmitry Vernigora <dmitry.vernigora@eleanorsoft.com>
2018-09-18 11:30:52 +0300

    fix routes

--- controller/AccountController.php
+++ controller/AccountController.php
@@ -9,7 +9,7 @@ class AccountController
 
     public function registrationAction()
     {
-
+        var_dump(1); exit;
         //include 'view/registerView.php';
         //include 'view/registerView.php';
     }
--- index.php
+++ index.php
@@ -1,30 +1,31 @@
 <?php
 
 require_once 'vendor/autoload.php';
+//require_once 'controller/HelloController.php';
 //require_once  'model/db.php';
 
-// Define the routes table
-$defaultAction = "indexAction";
-$routes = array(
-    '/\/hello\/(.+)/' => array('HelloController', 'helloAction'),
-	'/\/account\//' => array('AccountController', 'indexAction'),
-    '/\/account\/registration/' => array('AccountController', 'registrationAction'),
-);
 
-
-foreach ($routes as $url => $action) {
-    $matches = preg_match($url, $_SERVER['REQUEST_URI'], $params);
-    if ($matches > 0) {
-        var_dump($action);
-        $controller = new $action[0];
-        $controller->{$action[1]}($params);
-        break;
-    }
-//    else
-//    {
-//        $controller = new NotFoundController();
-//        $controller->indexAction();
-//        header('Location: /php');
-//    }
-
+   // $matches = preg_match($url, $_SERVER['REQUEST_URI'], $params);
+$path = str_replace('/php/', '', $_SERVER['REQUEST_URI']);
+$parts = array_filter(explode('/', $path));
+if (!count($parts))
+{
+    $parts[0] = 'hello';
+    $parts[1] = 'hello';
 }
+$params = array_slice($parts, 2);
+$temp = mb_convert_case($parts[0], MB_CASE_TITLE, "UTF-8");
+$temp .= 'Controller';
+
+if (!class_exists($temp)) {
+    var_dump(404); exit;
+}
+
+$controller = new $temp();
+$action =  $parts[1].'Action';
+
+if (!method_exists($temp, $action)) {
+    var_dump(404); exit;
+}
+
+$controller->{$action}($params);
